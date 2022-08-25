eda51117433949b0b5cc68cb4c98ece85b71152a
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-25 22:54:52 +0530

    Update BuildApp.groovy

--- vars/BuildApp.groovy
+++ vars/BuildApp.groovy
@@ -4,7 +4,7 @@ bat label: '', script: '''
 dotnet new tool-manifest
 dotnet tool install dotnet-ef
 dotnet tool restore
-dotnet ef migrartions add azsql --project DotNetCoreSqlDb/DotNetCoreSqlDb.csproj
+dotnet ef migrations add azsql --project DotNetCoreSqlDb/DotNetCoreSqlDb.csproj
 dotnet ef database update --project DotNetCoreSqlDb/DotNetCoreSqlDb.csproj
 dotnet build 
 '''
