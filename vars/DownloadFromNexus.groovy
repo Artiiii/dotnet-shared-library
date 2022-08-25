c4df0c0736f37bcb67e8ea88c339c40ea68371fc
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-25 23:09:44 +0530

    Update DownloadFromNexus.groovy

--- vars/DownloadFromNexus.groovy
+++ vars/DownloadFromNexus.groovy
@@ -2,6 +2,7 @@ def call()
 {
 
 powershell label:'', script:'''
+$version = Get-Content version.txt
 $publishUrl='http://localhost:8081/repository/dotnet-build-artifacts/dotnetcore/sample/'+$version+'/app.zip'
 Invoke-WebRequest -Method GET $publishUrl  -ContentType "application/json" -OutFile app.zip
 '''
