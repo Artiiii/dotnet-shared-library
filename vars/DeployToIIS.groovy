03baec93b33972cfa1ec470d9312d723bcbc940c
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-25 23:11:35 +0530

    Update DeployToIIS.groovy

--- vars/DeployToIIS.groovy
+++ vars/DeployToIIS.groovy
@@ -4,7 +4,7 @@ def call()
   Set-Item 'WSMan:localhost/client/trustedhosts' -value '40.114.48.109' -Force
   Enable-PSRemoting -Force
   $Pass=ConvertTo-SecureString -String 'Dotnet@123456' -AsPlainText -Force
-  $Credential=New-Object System.Management.Automation.PSCredential ("40.114.48.109\dotnet", $Pass)
+  $Credential=New-Object System.Management.Automation.PSCredential ("40.114.48.109\\dotnet", $Pass)
   $s=New-PSSession -ComputerName '40.114.48.109' -Credential $Credential
   Write-Output $s
   Copy-Item 'app.zip' 'C:/inetpub/wwwroot/' -ToSession $s
