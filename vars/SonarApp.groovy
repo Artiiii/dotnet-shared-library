958c0035fd4e63fdff60f4cc2ee5f35e8d2c3e49
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-25 15:29:25 +0530

    Update SonarApp.groovy

--- vars/SonarApp.groovy
+++ vars/SonarApp.groovy
@@ -1,4 +1,11 @@
 def call(login)
 {
-bat label: '', script: """mvn clean verify sonar:sonar -Dsonar.host.url=http://localhost:9000 -Dsonar.login=${login}"""
+bat label: '', script: """
+dotnet tool install --global JetBrains.dotCover.GlobalTool
+dotnet tool install --global dotnet-sonarscanner
+dotnet sonarscanner begin /k:"dotnetcoresqlsampleapp" /d:sonar.host.url="http://localhost:9000" /d:sonar.login=${login} /d:sonar.cs.vscoveragexml.reportsPaths=coverage.xml
+dotnet build --no-incremental
+dotnet dotcover test --dcReportType=HTML
+dotnet sonarscanner end /d:sonar.login=${login}
+"""
 }
