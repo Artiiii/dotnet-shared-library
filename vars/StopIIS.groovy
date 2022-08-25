fb233ada2e36b3a8cc552982762f51baadf2d015
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-25 22:41:48 +0530

    Update StopIIS.groovy

--- vars/StopIIS.groovy
+++ vars/StopIIS.groovy
@@ -7,15 +7,7 @@ def call()
  $Credential=New-Object System.Management.Automation.PSCredential ("40.114.48.109\dotnet", $Pass)
  $s=New-PSSession -ComputerName '40.114.48.109' -Credential $Credential      
  Invoke-Command -Session $s {
- $Exists=Get-IISSite -Name 'dotnetcore'
- if($Exists -ccontains "WARNING")
- {
- New-Item "C:\inetpub\wwwroot\dotnetcoresql" -ItemType Directory
- New-IISSite -Name "dotnetcore" -BindingInformation "*:82:" -PhysicalPath "C:\inetpub\wwwroot\dotnetcoresql" -Protocol http
- }
- else
- {
- Start-Website "dotnetcore" 
- }}
- '''  
+ Stop-Website "dotnetcore" 
+ }  
+ '''
 }
