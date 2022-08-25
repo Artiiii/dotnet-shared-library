16686c627c4fed0b3a48fdd85607f94a0acb8da1
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-25 21:37:48 +0530

    Update DownloadFromNexus.groovy

--- vars/DownloadFromNexus.groovy
+++ vars/DownloadFromNexus.groovy
@@ -1,4 +1,8 @@
 def call()
 {
-powershell label:'', script:'Invoke-WebRequest -Method GET "http://localhost:8081/service/rest/v1/search/assets/download?sort=version&repository=maven-snapshots&maven.groupId=com.javawebtutor&maven.artifactId=LoginWebApp&maven.extension=war"  -ContentType "application/json" -OutFile LoginWebApp.war'
+
+powershell label:'', script:'''
+$publishUrl='http://localhost:8081/repository/dotnet-build-artifacts/dotnetcore/sample/'+$version+'/app.zip'
+Invoke-WebRequest -Method GET $publishUrl  -ContentType "application/json" -OutFile app.zip
+'''
 }
