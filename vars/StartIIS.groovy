6f5c071fc9ffd99529836ded44d4386e707e28a0
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-25 22:22:42 +0530

    Create StartIIS.groovy

---
+++ vars/StartIIS.groovy
@@ -0,0 +1,12 @@
+def call()
+{
+  powershell label:'', script: '''
+  Set-Item 'WSMan:localhost/client/trustedhosts' -value '40.114.48.109' -Force
+  Enable-PSRemoting -Force
+  $Pass=ConvertTo-SecureString -String 'Dotnet@123456' -AsPlainText -Force
+  $Credential=New-Object System.Management.Automation.PSCredential ("40.114.48.109\dotnet", $Pass)
+  $s=New-PSSession -ComputerName '40.114.48.109' -Credential $Credential
+  Write-Output $s
+  Invoke-Command -Session $s {Start-Website "dotnetcore"}
+  '''
+}
