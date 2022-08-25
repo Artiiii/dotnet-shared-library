e932b030156d2709cf9838dc4b90bd34ca55f178
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-25 16:30:03 +0530

    Update DeployApp.groovy

--- vars/DeployApp.groovy
+++ vars/DeployApp.groovy
@@ -1,4 +1,7 @@
 def call()
 {
-powershell  label: '', script: 'Copy-Item "target/war.app.war" "C:/Users/Arti Pal/Documents/tools/apache-tomcat-10.0.23-windows-x64/apache-tomcat-10.0.23/webapps" -Force -Verbose'
+powershell  label: '', script: '''
+Invoke-WebRequest -Method Get 'http://localhost:8081/repository/dotnet-build-artifacts/dotnetcore/sample/${build}/app.zip' -OutFile 'app.zip'
+Get-Item .
+#Copy-Item "app.zip" "C:/Users/Arti Pal/Documents/tools/apache-tomcat-10.0.23-windows-x64/apache-tomcat-10.0.23/webapps" -Force -Verbose'
 }
