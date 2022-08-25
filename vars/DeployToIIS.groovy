aa5cca96c8e1ca4b682b54b9b56232e2a4fb4b5e
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-25 21:06:45 +0530

    Update DeployToIIS.groovy

--- vars/DeployToIIS.groovy
+++ vars/DeployToIIS.groovy
@@ -5,7 +5,7 @@ def call()
   Enable-PSRemoting -Force
   #$User='dotnet'
   $Pass=ConvertTo-SecureString -String 'Devops@123456' -AsPlainText -Force
-  $Credential=New-Object System.Management.Automation.PSCredential ("40.114.48.109\dotnet", $Pass)
+  $Credential=New-Object System.Management.Automation.PSCredential ("40.114.48.109\\dotnet", $Pass)
   $s=New-PSSession -ComputerName '40.114.48.109' -Credential $Credential
   Write-Output $s
   Copy-Item 'app.zip' 'C:/inetpub/wwwroot' -ToSession $s
