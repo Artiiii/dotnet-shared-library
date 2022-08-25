5c8cf006a5aa743fcf1241754a90be47ff883f80
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-25 22:47:44 +0530

    Update BuildApp.groovy

--- vars/BuildApp.groovy
+++ vars/BuildApp.groovy
@@ -1,6 +1,9 @@
 def call()
 {
 bat label: '', script: '''
+dotnet new tool-manifest
+dotnet tool install dotnet-ef
+dotnet tool restore
 dotnet ef migrartions add azsql
 dotnet ef database update
 dotnet build 
