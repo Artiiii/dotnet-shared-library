a02525c05fdf2b4a5fa51779c627e2ffee9e088b
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-25 16:18:57 +0530

    Update StoreToNexus.groovy

--- vars/StoreToNexus.groovy
+++ vars/StoreToNexus.groovy
@@ -1,4 +1,26 @@
-def call()
+def call(username, password, build)
 {
-  bat label: '', script: 'mvn clean deploy'
+  powershell label: '', script: '''
+  New-Item "PublishFolder" -itemType Directory
+  dotnet publish -o PublishFolder
+  Compress-Archive -Path PublishFolder/* -DestinationPath app.zip
+  
+  $publishUrl='http://localhost:8081/repository/dotnet-build-artifacts/dotnetcore/sample/${build}/app.zip'
+  $packageName = 'PublishFolder/app.zip'
+  $username= ${username}
+  $password= ${password}
+  $params = @{
+  UseBasicParsing = $true
+  Uri             = $publishUrl
+  Method          = "PUT"
+  InFile          = $packageName
+  Headers         = @{
+    ContentType   = "application/zip"
+    Authorization = "Basic $([System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes("$username`:$password")))" 
+  }
+  Verbose         = $true
+  }
+  Invoke-WebRequest @params
+  Remove-Item "PublishFolder"
+  '''
 }
