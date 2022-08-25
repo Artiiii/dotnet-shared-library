e0a5f9fa069a54aa955d27a99872d243b7ea62d8
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-25 20:39:37 +0530

    Update DeployToIIS.groovy

--- vars/DeployToIIS.groovy
+++ vars/DeployToIIS.groovy
@@ -4,8 +4,8 @@ powershell label:'', script: """
 Set-Item 'WSMan:localhost/client/trustedhosts' -value '40.114.48.109'
 Enable-PSRemoting -Force
 $User = 'dotnet'
-$PWord = ConvertTo-SecureString -String 'Devops@123456' -AsPlainText -Force
-$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
+$Pass = ConvertTo-SecureString -String 'Devops@123456' -AsPlainText -Force
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $Pass
 $s = New-PSSession -ComputerName '40.114.48.109' -Credential $Credential
 Copy-Item 'app.zip' 'C:/inetpub/wwwroot' -ToSession $s
 """
