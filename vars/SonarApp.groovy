3135221afce37c230b08df24be949fd20657e855
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-25 16:55:26 +0530

    Update SonarApp.groovy

--- vars/SonarApp.groovy
+++ vars/SonarApp.groovy
@@ -1,7 +1,7 @@
 def call(login)
 {
 bat label: '', script: """
-dotnet sonarscanner begin /k:"dotnetcoresqlsampleapp" /d:sonar.host.url="http://localhost:9000" /d:sonar.login=${login} /d:sonar.cs.vscoveragexml.reportsPaths=coverage.xml
+dotnet sonarscanner begin /k:"dotnetcoresqlsampleapp" /d:sonar.host.url="http://localhost:9000" /d:sonar.login=${login} /d:sonar.cs.dotcover.reportsPaths=dotCover.Output.html
 dotnet build --no-incremental
 dotnet dotcover test --dcReportType=HTML
 dotnet sonarscanner end /d:sonar.login=${login}
