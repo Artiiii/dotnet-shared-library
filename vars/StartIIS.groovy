3bec9d22443e9373008d71b03762f644530539b5
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-26 11:18:53 +0530

    Update StartIIS.groovy

--- vars/StartIIS.groovy
+++ vars/StartIIS.groovy
@@ -1,12 +1,12 @@
 def call()
 {
   powershell label:'', script: '''
-  Set-Item 'WSMan:localhost/client/trustedhosts' -value '40.114.48.109' -Force
+  Set-Item 'WSMan:localhost/client/trustedhosts' -value '104.41.133.141' -Force
   Enable-PSRemoting -Force
   $Pass=ConvertTo-SecureString -String 'Devops@123456' -AsPlainText -Force
   $Credential=New-Object System.Management.Automation.PSCredential ("104.41.133.141\\dotnet", $Pass)
   $s=New-PSSession -ComputerName '104.41.133.141' -Credential $Credential
   Write-Output $s
-  Invoke-Command -Session $s {Start-Website "dotnetcore" -Force}
+  Invoke-Command -Session $s {Start-Website "dotnetcore"}
   '''
 }
