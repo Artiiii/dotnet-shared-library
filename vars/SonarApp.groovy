f0723e0bef2b544c0849eb2645ff45cce623fba8
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-26 10:21:30 +0530

    Update SonarApp.groovy

--- vars/SonarApp.groovy
+++ vars/SonarApp.groovy
@@ -1,6 +1,7 @@
 def call(login)
 {
-powershell label: '', script: """
+bat label: '', script: """
+set PATH=%PATH%;C:/Program Files/dotnet/
 dotnet tool install dotnet-sonarscanner
 dotnet tool restore
 dotnet dotnet-sonarscanner begin /k:'dotnetcoresqlsampleapp' /d:sonar.host.url='http://localhost:9000' /d:sonar.login=${login}
