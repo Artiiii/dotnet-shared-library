9007eec6876912ddabdebbcfb5c0a9b1b1d471d5
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-26 10:35:32 +0530

    Update StoreToNexus.groovy

--- vars/StoreToNexus.groovy
+++ vars/StoreToNexus.groovy
@@ -1,10 +1,12 @@
 def call()
 {
+  bat label: '', script: '''
+  set PATH=%PATH%;C:/Program Files/dotnet/
+  mkdir PublishFolder
+  dotnet publish -o PublishFolder
+  '''
   powershell label: '', script: '''
- #set PATH=%PATH%;C:/Program Files/dotnet/
- #New-Item "PublishFolder" -itemType Directory
- #dotnet publish -o PublishFolder
- #Compress-Archive -Path PublishFolder/* -DestinationPath app.zip
+  Compress-Archive -Path PublishFolder/* -DestinationPath app.zip
   $version = Get-Content version.txt
   $publishUrl='http://localhost:8081/repository/dotnet-build-artifacts/dotnetcore/sample/'+$version+'/app.zip'
   $packageName = 'app.zip'
