64989b8b6383e25a1c9b174dfcd49311b65fc311
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-26 21:42:29 +0530

    Update StoreToNexus.groovy

--- vars/StoreToNexus.groovy
+++ vars/StoreToNexus.groovy
@@ -5,13 +5,15 @@ def call(username, password)
   mkdir PublishFolder
   dotnet publish -o PublishFolder
   '''
+  powershell label: '', script: """
+  $env.$username = ${username}
+  $env.$password = ${password}
+  """
   powershell label: '', script: '''
   Compress-Archive -Path PublishFolder/* -DestinationPath app.zip
   $version = Get-Content version.txt
   $publishUrl='http://localhost:8081/repository/dotnet-build-artifacts/dotnetcore/sample/'+$version+'/app.zip'
   $packageName = 'app.zip'
-  $username = $username
-  $password = $password
   $params = @{
   UseBasicParsing = ${true}
   Uri             = $publishUrl
