7364f24c47d3aeaa29b898ee9056c137bf6d757a
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-26 18:38:00 +0530

    Delete DeployApp.groovy

--- vars/DeployApp.groovy
+++
@@ -1,7 +0,0 @@
-def call()
-{
-powershell  label: '', script: '''
-Invoke-WebRequest -Method Get 'http://localhost:8081/repository/dotnet-build-artifacts/dotnetcore/sample/${build}/app.zip' -OutFile 'app.zip'
-Get-Item .
-#Copy-Item "app.zip" "C:/Users/Arti Pal/Documents/tools/apache-tomcat-10.0.23-windows-x64/apache-tomcat-10.0.23/webapps" -Force -Verbose'
-}
