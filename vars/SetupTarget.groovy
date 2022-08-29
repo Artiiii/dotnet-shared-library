99d838185777f95abe94a32e54952c22f515e0eb
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-29 23:28:43 +0530

    Update SetupTarget.groovy

--- vars/SetupTarget.groovy
+++ vars/SetupTarget.groovy
@@ -44,16 +44,16 @@ $iisWebsiteName = "dotnetcoresql"
 $iisWebsiteBindings = @(  
    @{protocol="http";bindingInformation="*:80:"}
 )
-if (!(Test-Path IIS:\AppPools\$iisAppPoolName -pathType container))  
+if (!(Test-Path IIS:/AppPools/$iisAppPoolName -pathType container))  
 {  
-New-Item IIS:\AppPools\$iisAppPoolName  
-Set-ItemProperty IIS:\AppPools\$iisAppPoolName -name "managedRuntimeVersion" -value $iisAppPoolDotNetVersion  
+New-Item IIS:/AppPools/$iisAppPoolName  
+Set-ItemProperty IIS:/AppPools/$iisAppPoolName -name "managedRuntimeVersion" -value $iisAppPoolDotNetVersion  
 }    
-if (!(Test-Path IIS:\Sites\$iisWebsiteName -pathType container))  
+if (!(Test-Path IIS:/Sites/$iisWebsiteName -pathType container))  
 {  
 Remove-Website "Default Web Site"
-New-Item IIS:\Sites\$iisWebsiteName -bindings $iisWebsiteBindings -physicalPath $iisWebsiteFolderPath  
-Set-ItemProperty IIS:\Sites\$iisWebsiteName -name applicationPool -value $iisAppPoolName  
+New-Item IIS:/Sites/iisWebsiteName -bindings $iisWebsiteBindings -physicalPath $iisWebsiteFolderPath  
+Set-ItemProperty IIS:/ites/iisWebsiteName -name applicationPool -value $iisAppPoolName  
 }  
 Stop-WebSite "dotnetcore"
 }
