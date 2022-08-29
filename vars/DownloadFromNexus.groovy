d2094223fb861213016b4e08c83186f76b3187c0
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-30 01:15:00 +0530

    Update DownloadFromNexus.groovy

--- vars/DownloadFromNexus.groovy
+++ vars/DownloadFromNexus.groovy
@@ -5,5 +5,8 @@ powershell label:'', script:'''
 $version = Get-Content version.txt
 $publishUrl='http://localhost:8081/repository/dotnet-build-artifacts/dotnetcore/sample/'+$version+'/app.zip'
 Invoke-WebRequest -Method GET $publishUrl  -ContentType "application/json" -OutFile app.zip
+Write-Output "Downloading Dotnet bundle"
+Invoke-WebRequest -Method GET 'http://localhost:8081/repository/dotnet-build-artifacts/tools/dotnet-hosting-6.0.8-win.exe  -ContentType "application/json" -OutFile dotnet-hosting-6.0.8-win.exe
+Write-Output "Downloaded Dotnet bundle"
 '''
 }
