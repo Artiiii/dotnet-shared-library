02b38462231b2c4ab5b64753b70226d140ffc9b9
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-29 17:10:32 +0530

    Update StoreToNexus.groovy

--- vars/StoreToNexus.groovy
+++ vars/StoreToNexus.groovy
@@ -8,22 +8,24 @@ def call()
  
   powershell label: '', script: '''
   Compress-Archive -Path PublishFolder/* -DestinationPath app.zip
-  $version = Get-Content version.txt
+c $version = Get-Content version.txt
   $publishUrl='http://localhost:8081/repository/dotnet-build-artifacts/dotnetcore/sample/'+$version+'/app.zip'
+  $packageName = 'app.zip'
   $username = 'admin'
   $password = 'admin'
-  $packageName = 'app.zip'
   $params = @{
-  UseBasicParsing = ${true}
+  UseBasicParsing = $true
   Uri             = $publishUrl
-  Method          = 'PUT'
+  Method          = "PUT"
   InFile          = $packageName
   Headers         = @{
-    ContentType   = 'application/zip'
-    Authorization = 'Basic $([System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes("admin:admin")))' 
+    ContentType   = "application/zip"
+    Authorization = "Basic $([System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes("$username`:$password")))" 
   }
-  Verbose         = ${true}
+  Verbose         = $true
   }
   Invoke-WebRequest @params
+  '''
+}
   '''
 }
