7b130c80cfc62264e4f392cae3e2c1e31109a698
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-29 19:03:29 +0530

    Update SetupTarget.groovy

--- vars/SetupTarget.groovy
+++ vars/SetupTarget.groovy
@@ -1,6 +1,6 @@
 def call(password)
 {
- powershell label: '', script """
+ powershell label: '', script: """
  $ip = Get-Content output.txt
  $password = ${password}
  Set-Item 'WSMan:localhost/client/trustedhosts' -value $ip -Force
