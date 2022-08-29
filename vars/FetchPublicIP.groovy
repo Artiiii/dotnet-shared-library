70c45d62e0e05fff55ca7e204618e05e18464532
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-29 20:17:36 +0530

    Update FetchPublicIP.groovy

--- vars/FetchPublicIP.groovy
+++ vars/FetchPublicIP.groovy
@@ -6,7 +6,7 @@ az login --service-principal -u %azcon_CLIENT_ID% -p %azcon_CLIENT_SECRET% -t %a
 az vm list-ip-addresses --resource-group ranjith --name dotnetcoresamplevm > fetchpublicip.json
 '''
 powershell label: '', script: """ 
-
+az vm list-ip-addresses --resource-group ranjith --name dotnetcoresamplevm > fetchpublicip.json
 (Get-Content fetchpublicip.json | ConvertFrom-Json).virtualMachine.network.publicIpAddresses.ipAddress > output.txt
 Write-Output "Came"
 Get-Content output.txt
