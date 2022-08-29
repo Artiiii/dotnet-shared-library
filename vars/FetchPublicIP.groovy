82506386d5c7ba0828e0e874db22a04020793a77
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-29 20:22:33 +0530

    Update FetchPublicIP.groovy

--- vars/FetchPublicIP.groovy
+++ vars/FetchPublicIP.groovy
@@ -2,13 +2,11 @@ def call()
 {
 bat label: '', script: '''
 set PATH=%PATH%;C:/Program Files (x86)/Microsoft SDKs/Azure/CLI2/wbin
-az login --service-principal -u %azcon_CLIENT_ID% -p %azcon_CLIENT_SECRET% -t %azcon_TENANT_ID%
+az login --service-principal -u %azcon_CLIENT_ID% -p %azcon_CLIENT_SECRET% -t %azcon_TENANT_ID% && az vm list-ip-addresses --resource-group ranjith --name dotnetcoresamplevm > fetchpublicip.json
 echo "Hello"
 az vm list-ip-addresses --resource-group ranjith --name dotnetcoresamplevm > fetchpublicip.json
 '''
 powershell label: '', script: """ 
-set PATH=%PATH%;"C:/Program Files (x86)/Microsoft SDKs/Azure/CLI2/wbin"
-az vm list-ip-addresses --resource-group ranjith --name dotnetcoresamplevm > fetchpublicip.json
 (Get-Content fetchpublicip.json | ConvertFrom-Json).virtualMachine.network.publicIpAddresses.ipAddress > output.txt
 Write-Output "Came"
 Get-Content output.txt
