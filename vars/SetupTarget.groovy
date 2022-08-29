87a8511cd2bf0b3bc22779a23af70476c9f8bc91
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-29 23:05:41 +0530

    Update SetupTarget.groovy

--- vars/SetupTarget.groovy
+++ vars/SetupTarget.groovy
@@ -13,32 +13,48 @@ def call(password)
  
  Write-Output "#------ INSTALLING DOTNET CORE HOSTING BUNDLE------#"
  
- $s=New-PSSession -ComputerName $myip -Credential $Credential   
+ $s=New-PSSession -ComputerName $myip -Credential $Credential 
+ $exists = Invoke-Command -Session $s {Test-Path 'C:/inetpub/wwwroot/dotnet-hosting-6.0.8-win.exe'}
+ Write-Output $exists
+ if($exists -eq "True")
+ {
+ Write-Output "File Exists"
+ }
+ else{
  Copy-Item 'dotnet-hosting-6.0.8-win.exe' 'C:/inetpub/wwwroot' -ToSession $s
- Invoke-Command -Session $s {
  $pathvargs = { & 'C:/inetpub/wwwroot/dotnet-hosting-6.0.8-win.exe' /S /v/qn }
  Invoke-Command -ScriptBlock $pathvargs
+ }
+
  
  
  Write-Output "#------- CREATE IIS WEBSITE OR STOP WEBSITE iF ALREADY EXISTS------------------#"
-
- $Exists=Get-WebSite -Name 'dotnetcore'
- Write-Output $Exists
- if(!$Exists)
- {
  if (Test-Path "C:/inetpub/wwwroot/dotnetcoresql" ) {
  
      Write-Host "Folder Exists"
      Remove-Item "C:/inetpub/wwwroot/dotnetcoresql" -Force
  }
  New-Item "C:/inetpub/wwwroot/dotnetcoresql" -ItemType Directory
- New-WebSite -Name "dotnetcore" -Port "98" -PhysicalPath "C:/inetpub/wwwroot/dotnetcoresql"
- }
- else
- {
- Stop-WebSite "dotnetcore"
- Write-Output "Website Stopped"
- }
- }
+
+Import-Module WebAdministration    
+$iisAppPoolName = "dotnetcoresql"  
+$iisAppPoolDotNetVersion = "v4.0"    
+$iisWebsiteFolderPath = "C:\inetpub\wwwroot\dotnetcoresql"  
+$iisWebsiteName = "dotnetcoresql"  
+$iisWebsiteBindings = @(  
+   @{protocol="http";bindingInformation="*:80:"}
+)
+if (!(Test-Path IIS:\AppPools\$iisAppPoolName -pathType container))  
+{  
+New-Item IIS:\AppPools\$iisAppPoolName  
+Set-ItemProperty IIS:\AppPools\$iisAppPoolName -name "managedRuntimeVersion" -value $iisAppPoolDotNetVersion  
+}    
+if (!(Test-Path IIS:\Sites\$iisWebsiteName -pathType container))  
+{  
+New-Item IIS:\Sites\$iisWebsiteName -bindings $iisWebsiteBindings -physicalPath $iisWebsiteFolderPath  
+Set-ItemProperty IIS:\Sites\$iisWebsiteName -name applicationPool -value $iisAppPoolName  
+}  
+Stop-WebSite "dotnetcore"
+}
  '''
 }
