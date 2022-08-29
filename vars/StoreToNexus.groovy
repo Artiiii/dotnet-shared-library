41486ca714920c2bc87e108dc37fb03c7d923e79
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-29 16:58:38 +0530

    Update StoreToNexus.groovy

--- vars/StoreToNexus.groovy
+++ vars/StoreToNexus.groovy
@@ -1,15 +1,11 @@
-def call(username, password)
+def call()
 {
   bat label: '', script: '''
   set PATH=%PATH%;C:/Program Files/dotnet/
   mkdir PublishFolder
   dotnet publish -o PublishFolder
   '''
-  powershell label: '', script: """
-  \$user = ${username}
-  \$pass = ${password}
-  Wrote-Output "Completed"
-  """
+ 
   powershell label: '', script: '''
   Compress-Archive -Path PublishFolder/* -DestinationPath app.zip
   $version = Get-Content version.txt
@@ -22,7 +18,7 @@ def call(username, password)
   InFile          = $packageName
   Headers         = @{
     ContentType   = 'application/zip'
-    Authorization = 'Basic $([System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes("$user`:$pass")))' 
+    Authorization = 'Basic $([System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes("admin`:admin")))' 
   }
   Verbose         = ${true}
   }
