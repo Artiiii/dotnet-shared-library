1297b6a770d57fec3e01b35bd088c152d33924e3
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-08-29 21:00:00 +0530

    Update DeployToIIS.groovy

--- vars/DeployToIIS.groovy
+++ vars/DeployToIIS.groovy
@@ -1,11 +1,12 @@
 def call()
 {
  powershell label:'', script: '''
-  Set-Item 'WSMan:localhost/client/trustedhosts' -value '104.41.133.141' -Force
+  $myip=Get-Content output.txt
+  Set-Item 'WSMan:localhost/client/trustedhosts' -value $myip -Force
   Enable-PSRemoting -Force
   $Pass=ConvertTo-SecureString -String 'Devops@123456' -AsPlainText -Force
-  $Credential=New-Object System.Management.Automation.PSCredential ("104.41.133.141\\dotnet", $Pass)
-  $s=New-PSSession -ComputerName '104.41.133.141' -Credential $Credential
+  $Credential=New-Object System.Management.Automation.PSCredential ("$myip\\dotnet", $Pass)
+  $s=New-PSSession -ComputerName $myip -Credential $Credential
   Write-Output $s
   Invoke-Command -Session $s {Remove-Item 'C:/inetpub/wwwroot/app.zip'}
   Copy-Item 'app.zip' 'C:/inetpub/wwwroot/' -ToSession $s
