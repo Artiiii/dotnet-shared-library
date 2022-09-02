b8c70f3dea28a334e05f7fa96b2cc7b48f842f3b
Artiiii <31152341+Artiiii@users.noreply.github.com>
2022-09-02 11:40:15 +0530

    Update DeployToIIS.groovy

--- vars/DeployToIIS.groovy
+++ vars/DeployToIIS.groovy
@@ -3,24 +3,18 @@ def call()
  powershell label:'', script: '''
   $myip=Get-Content output.txt
   Write-Output $myip
-  Set-Item 'WSMan:localhost/client/trustedhosts' -value "$myip" -Force
-  $user = "$myip\\dotnet"
+  $user = "dotnet"
   Write-Output $user
-  Enable-PSRemoting -Force
   $Pass=ConvertTo-SecureString -String 'Devops@123456' -AsPlainText -Force
   $Credential=New-Object System.Management.Automation.PSCredential ($user, $Pass)
   $s=New-PSSession -ComputerName $myip -Credential $Credential
   Write-Output $s
   Invoke-Command -Session $s {
-  
- if (Test-Path "C:/inetpub/wwwroot/app.zip" ) {
      Remove-Item 'C:/inetpub/wwwroot/app.zip'
- }
 }
   Copy-Item 'app.zip' 'C:/inetpub/wwwroot/' -ToSession $s
   Invoke-Command -Session $s {
-  if (Test-Path "C:/inetpub/wwwroot/dotnetcoresql/*" ) {
-  Remove-Item 'C:/inetpub/wwwroot/dotnetcoresql/*' -Recurse }
+  Remove-Item 'C:/inetpub/wwwroot/dotnetcoresql/*' -Recurse 
   Expand-Archive -Path 'C:/inetpub/wwwroot/app.zip' -DestinationPath 'C:/inetpub/wwwroot/dotnetcoresql'}
 '''
 }
